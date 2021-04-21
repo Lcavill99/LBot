0c3afe00f609137ad14dc8a6b69d39518fd1ec1f
Lcavill99 <44065767+Lcavill99@users.noreply.github.com>
2021-04-21 15:08:19 +0100

    Created a BuildingManager

---
+++ LBot/Source/BuildingManager.cpp
@@ -0,0 +1,16 @@
+/*
+ *
+ * Handles all building logic
+ *
+ */
+#include "buildingManager.h"
+
+using namespace BWAPI;
+
+/*
+ * Remove unit
+ */
+void BuildingManager::removeUnit(BWAPI::Unit u)
+{
+
+}
---
+++ LBot/Source/BuildingManager.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <BWAPI.h>
+
+class BuildingManager
+{
+
+public:
+	void BuildingManager::removeUnit(BWAPI::Unit u);
+};
