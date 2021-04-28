c905ad529ad89d931beff85b5cc11f50ead0e010
Lcavill99 <44065767+Lcavill99@users.noreply.github.com>
2021-04-28 15:08:02 +0100

    Added multiple functions handling workers

--- LBot/Source/WorkerManager.cpp
+++ LBot/Source/WorkerManager.cpp
@@ -8,7 +8,7 @@
 using namespace BWAPI;
 
 /*
- * Select a worker
+ * Select a worker out of all workers
  */
 BWAPI::Unit WorkerManager::getWorker()
 {
@@ -16,18 +16,66 @@ BWAPI::Unit WorkerManager::getWorker()
 
 	for (auto &u : Broodwar->self()->getUnits())
 	{
-		if (u->getType().isWorker() && !u->isConstructing())
+		if (u->getType().isWorker())
 		{
-			worker = u;
-			return worker;
-		}
+			if (!u->isConstructing() && !u->isGatheringGas() && !u->isCarryingMinerals() && !u->isCarryingGas())
+			{
+				worker = u;
+				return worker;
+			}
+		}		
 	}
+	Broodwar->printf("WorkerManager Error: No available workers");
+	return NULL;
 }
 
 /*
- * Remove unit
+ * Select a worker that can build from specific unitset
  */
-void WorkerManager::removeUnit(BWAPI::Unit u)
+BWAPI::Unit WorkerManager::getWorkerFromSet(BWAPI::Unitset set)
+{
+	BWAPI::Unit worker;
+
+	for (auto &u : set)
+	{
+		if (!u->isConstructing() && !u->isGatheringGas() && (!u->isCarryingMinerals() || !u->isCarryingGas()))
+		{
+			worker = u;
+			return worker;
+		}		
+	}
+	Broodwar->printf("WorkerManager Error: No available workers");
+	return NULL;
+}
+
+/*
+ * Add unit to Unitset
+ */
+void WorkerManager::addUnit(BWAPI::Unitset set, BWAPI::Unit u)
+{
+	set.insert(u);
+}
+
+/*
+ * Remove unit from unitset
+ */
+void WorkerManager::removeUnit(BWAPI::Unitset set, BWAPI::Unit u)
+{
+	set.erase(u);
+}
+
+/*
+ * Worker gathering minerals
+ */
+void WorkerManager::gatherMinerals()
+{
+
+}
+
+/*
+ * Worker gathering gas
+ */
+void WorkerManager::gatherGas()
 {
 
 }
--- LBot/Source/WorkerManager.h
+++ LBot/Source/WorkerManager.h
@@ -2,9 +2,13 @@
 #include <BWAPI.h>
 
 class WorkerManager
-{
+{	
 
-public:
+public:	
 	BWAPI::Unit WorkerManager::getWorker();
-	void WorkerManager::removeUnit(BWAPI::Unit u);
+	BWAPI::Unit WorkerManager::getWorkerFromSet(BWAPI::Unitset set);
+	void WorkerManager::addUnit(BWAPI::Unitset set, BWAPI::Unit u);
+	void WorkerManager::removeUnit(BWAPI::Unitset set, BWAPI::Unit u);	
+	void WorkerManager::gatherMinerals();
+	void WorkerManager::gatherGas();
 };
