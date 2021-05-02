190ee7a9ac318926ba524da4135e6988818faa7d
Lcavill99 <44065767+Lcavill99@users.noreply.github.com>
2021-05-02 13:15:34 +0100

    Added gathering gas and minerals functionality, tried updating add and remove unit functionality to get it to work

--- LBot/Source/WorkerManager.cpp
+++ LBot/Source/WorkerManager.cpp
@@ -6,6 +6,7 @@
 #include "WorkerManager.h"
 
 using namespace BWAPI;
+using namespace Filter;
 
 /*
  * Select a worker out of all workers
@@ -32,11 +33,11 @@ BWAPI::Unit WorkerManager::getWorker()
 /*
  * Select a worker that can build from specific unitset
  */
-BWAPI::Unit WorkerManager::getWorkerFromSet(BWAPI::Unitset set)
+BWAPI::Unit WorkerManager::getWorkerFromSet(BWAPI::Unitset* set)
 {
 	BWAPI::Unit worker;
 
-	for (auto &u : set)
+	for (auto &u : *set)
 	{
 		if (!u->isConstructing() && !u->isGatheringGas() && (!u->isCarryingMinerals() || !u->isCarryingGas()))
 		{
@@ -51,31 +52,55 @@ BWAPI::Unit WorkerManager::getWorkerFromSet(BWAPI::Unitset set)
 /*
  * Add unit to Unitset
  */
-void WorkerManager::addUnit(BWAPI::Unitset set, BWAPI::Unit u)
+void WorkerManager::addUnit(BWAPI::Unitset* set, BWAPI::Unit* u)
 {
-	set.insert(u);
+	set->insert(*u);
 }
 
 /*
  * Remove unit from unitset
  */
-void WorkerManager::removeUnit(BWAPI::Unitset set, BWAPI::Unit u)
+void WorkerManager::removeUnit(BWAPI::Unitset* set, BWAPI::Unit* u)
 {
-	set.erase(u);
+	set->erase(*u);
 }
 
 /*
  * Worker gathering minerals
  */
-void WorkerManager::gatherMinerals()
+void WorkerManager::gatherMinerals(BWAPI::Unit* u)
 {
-
+	if ((*u)->isCarryingMinerals())
+	{
+		(*u)->returnCargo();
+	}
+	else if (!(*u)->getPowerUp()) // The worker cannot harvest anything if it is carrying a powerup such as a flag
+	{
+		// Harvest from the nearest mineral patch 
+		if (!(*u)->gather((*u)->getClosestUnit(IsMineralField)))
+		{
+			// If the call fails, then print the last error message
+			Broodwar << Broodwar->getLastError() << std::endl;
+		}
+	}
 }
 
 /*
  * Worker gathering gas
  */
-void WorkerManager::gatherGas()
+void WorkerManager::gatherGas(BWAPI::Unitset* set, BWAPI::Unit* u)
 {
-
+	if ((*u)->isCarryingGas())
+	{
+		(*u)->returnCargo();
+	}
+	else if (!(*u)->getPowerUp()) // The worker cannot harvest anything if it is carrying a powerup such as a flag
+	{
+		// gather from nearest refinery
+		if (!(*u)->gather((*u)->getClosestUnit(IsRefinery)))
+		{
+			// If the call fails, then print the last error message
+			Broodwar << Broodwar->getLastError() << std::endl;
+		}
+	}
 }
--- LBot/Source/WorkerManager.h
+++ LBot/Source/WorkerManager.h
@@ -6,9 +6,9 @@ class WorkerManager
 
 public:	
 	BWAPI::Unit WorkerManager::getWorker();
-	BWAPI::Unit WorkerManager::getWorkerFromSet(BWAPI::Unitset set);
-	void WorkerManager::addUnit(BWAPI::Unitset set, BWAPI::Unit u);
-	void WorkerManager::removeUnit(BWAPI::Unitset set, BWAPI::Unit u);	
-	void WorkerManager::gatherMinerals();
-	void WorkerManager::gatherGas();
+	BWAPI::Unit WorkerManager::getWorkerFromSet(BWAPI::Unitset* set);
+	void WorkerManager::addUnit(BWAPI::Unitset* set, BWAPI::Unit* u);
+	void WorkerManager::removeUnit(BWAPI::Unitset* set, BWAPI::Unit* u);	
+	void WorkerManager::gatherMinerals(BWAPI::Unit* u);
+	void WorkerManager::gatherGas(BWAPI::Unitset* set, BWAPI::Unit* u);
 };
