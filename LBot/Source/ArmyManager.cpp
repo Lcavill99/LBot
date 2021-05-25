95ab6bba09e09cb3aa43b142712ae2f7093c1cac
Lcavill99 <44065767+Lcavill99@users.noreply.github.com>
2021-05-25 12:47:12 +0100

    Removed add and remove unit functions

--- LBot/Source/ArmyManager.cpp
+++ LBot/Source/ArmyManager.cpp
@@ -9,22 +9,6 @@ using namespace BWAPI;
 using namespace Filter;
 
 /*
- * Add unit to Unitset
- */
-void ArmyManager::addUnit(BWAPI::Unitset* set, BWAPI::Unit* u)
-{
-	set->insert(*u);
-}
-
-/*
- * Remove unit from unitset
- */
-void ArmyManager::removeUnit(BWAPI::Unitset* set, BWAPI::Unit* u)
-{
-	set->erase(*u);
-}
-
-/*
  * Army group attacking
  */
 void ArmyManager::groupAttack(BWAPI::Unitset set)
