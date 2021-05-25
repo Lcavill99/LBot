e80ede937112f171a4fad70a88d64827fad798fd
Lcavill99 <44065767+Lcavill99@users.noreply.github.com>
2021-05-25 12:47:22 +0100

    Removed add and remove unit functions

--- LBot/Source/BuildingManager.cpp
+++ LBot/Source/BuildingManager.cpp
@@ -8,22 +8,6 @@
 using namespace BWAPI;
 
 /*
- * Add unit to Unitset
- */
-void BuildingManager::addUnit(BWAPI::Unitset* set, BWAPI::Unit* u)
-{
-	set->insert(*u);
-}
-
-/*
- * Remove unit from unitset
- */
-void BuildingManager::removeUnit(BWAPI::Unitset* set, BWAPI::Unit* u)
-{
-	set->erase(*u);
-}
-
-/*
  * Recruit units
  */
 void BuildingManager::recruitUnit(BWAPI::Unit u)
--- LBot/Source/BuildingManager.h
+++ LBot/Source/BuildingManager.h
@@ -5,8 +5,6 @@ class BuildingManager
 {
 
 public:
-	void BuildingManager::addUnit(BWAPI::Unitset* set, BWAPI::Unit* u);
-	void BuildingManager::removeUnit(BWAPI::Unitset* set, BWAPI::Unit* u);
 	void BuildingManager::recruitUnit(BWAPI::Unit u);
 	void BuildingManager::researchTech(BWAPI::Unit u);
 };
